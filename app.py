8fbb79b5f1404111a4e26d8836e22e65e2cc659b
Jose Ignacio Ferrer Vera <joseferrertec@gmail.com>
2022-10-18 22:40:21 -0500

    first Commit

---
+++ app.py
@@ -0,0 +1,152 @@
+#from pyexpat import model
+from pandas import read_csv, concat
+from pickle import load, dump
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+
+#Import Flask modules
+from flask import Flask, request, render_template, jsonify
+
+import os
+
+#Initialize Flask and set the template folder to "template"
+app = Flask(__name__, template_folder = 'template')
+UPLOAD_FOLDER = 'static/files'
+app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
+#Open our model 
+model = load(open('model.pkl','rb'))
+tfidfv = load(open('wordvector.mkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#Set a post method to yield predictions on page
+@app.route('/', methods = ['POST'])
+def predict():
+    #model = pickle.load(open('model.pkl','rb'))
+    #tfidf = pickle.load(open('wordvector.mkl','rb'))
+    #obtain all form values and place them in an array, convert into integers
+    features = tfidfv.transform(request.form.values())
+    #Combine them all into a final numpy array
+    #final_features = [np.array(features)]
+    #predict the price given the values inputted by user
+    prediction = model.predict(features)
+    
+    #Round the output to 2 decimal places
+    #output = round(prediction[0], 2)
+    
+    #If the output is negative, the values entered are unreasonable to the context of the application
+    #If the output is greater than 0, return prediction
+    """
+    if output < 0:
+        return render_template('index.html', prediction_text = "Predicted Price is negative, values entered not reasonable")
+    elif output >= 0:
+        """
+    return render_template('index.html', prediction_text = 'The review is: ${}'.format(prediction))   
+
+#Set the model
+@app.route('/csv', methods = ['POST'])
+def feed():
+    #print(request.files)
+    uploaded_file = request.files['filename']
+    #print(uploaded_file.filename)
+    if uploaded_file.filename != '':
+        try:
+            #print("Trying")
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+            #print("Path: ",file_path)
+            # set the file path
+            uploaded_file.save(file_path)
+            #print("UploadedFile:", uploaded_file.filename)
+            #redirect(url_for('index'))
+            #print("About to read")
+            dfreview = read_csv(file_path)
+            #print("File Read")
+            #data_top = dfreview.head() 
+            # display 
+            print("len: ", len(dfreview.columns))
+            if len(dfreview.columns) != 2:
+                raise Exception('Not the right number of columns')
+
+            review = dfreview.columns[0]
+            sentiment = dfreview.columns[1]
+            print("Columnas guardadas")
+            #for col in dfreview.columns:
+            #    print(col)
+            print("Review: ", review)
+            print("Sentiment: ", sentiment)
+            #sentpos, sentneg = "", ""
+
+            #print(dfreview.value_counts(sentiment))
+            #sentlist = dfreview[sentiment].tolist()
+            #print(sentlist)
+            #print("")
+            sentvals = dfreview[sentiment].unique()
+            print("Sentvals seteados")
+            
+            if len(sentvals) != 2:
+                raise Exception('Not the right number of sentiments')
+
+            sentpos = sentvals[0]
+            sentneg = sentvals[1]
+            #slices o particiones para crear un set desbalanceado
+            dfpositivos = dfreview[dfreview[sentiment]==sentpos][:9000]
+            dfnegativos = dfreview[dfreview[sentiment]==sentneg][:1000]
+            print("Los negas: ",dfnegativos)
+            dfreviewdes = concat([dfpositivos,dfnegativos])
+
+            #Se hace un undersampling para balancear el dataset partido
+            rus = RandomUnderSampler()
+            dfreviewbal, dfreviewbal[sentiment] = rus.fit_resample(dfreviewdes[[review]],dfreviewdes[sentiment])
+
+            #se divide en un conjunto de entrenamiento y otro de prueba
+            train, test=train_test_split(dfreviewbal, test_size=0.33, random_state=42)
+
+            #Se llenan los conjuntos de entrenamiento y prueba en valor(review) y output(sentiment)
+            trainX, trainY=train[review], train[sentiment]
+            testX, testY=test[review], test[sentiment]
+            """    
+            #ejemplo de manejo de texto a representacion numerica
+            text = ["I love writing code in Python. I love Python code",
+                    "I hate writing code in Java. I hate Java code"]
+            #Ejemplo para crear una matriz de conteo de frecuencias
+            df = pd.DataFrame({review: ['review1', 'review2'], 'text':text})
+            cv = CountVectorizer(stop_words='english')
+            cv_matrix = cv.fit_transform(df['text'])
+            df_dtm = pd.DataFrame(cv_matrix.toarray(), index=df[review].values, columns=cv.get_feature_names_out())
+            #ejemplo para crear una matriz con valores tfidf
+            tfidf = TfidfVectorizer(stop_words='english')
+            tfidf_matrix = tfidf.fit_transform(df['text'])
+            df_dtm = pd.DataFrame(tfidf_matrix.toarray(), index=df[review].values, columns=tfidf.get_feature_names_out())
+            """
+
+            #creamos y llenamos los conjuntos de valores que vamos a usar para los modelos y creamos nuestra bolsa de palabras en base al conjunto de entrenamiento
+            tfidf = TfidfVectorizer(stop_words='english')
+            trainXVector = tfidf.fit_transform(trainX)
+            testXVector = tfidf.transform(testX)
+
+            #SVM
+            svc = SVC(kernel='linear')
+            svc.fit(trainXVector,trainY)
+
+            #save model
+            dump(svc, open('model.pkl', 'wb'))
+            dump(tfidf, open('wordvector.mkl','wb'))
+            global model
+            global tfidfv
+            model = svc
+            tfidfv = tfidf
+            
+            return render_template('index.html', feed_result = 'The model was feed with: ${}'.format(uploaded_file.filename)) 
+        except Exception:
+            print(Exception)
+            return render_template('index.html', feed_result = 'Invalid file provided')
+    else:
+        return render_template('index.html', feed_result = 'No valid file provided')
+
+#Run app
+if __name__ == "__main__":
+    app.run(debug=True)
